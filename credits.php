08bcfcfcd738c401a69f4fd272aadef947d71a1a
Fabrice <19834776+fabricelallemand15@users.noreply.github.com>
2022-12-14 11:41:55 +0100

    Licence et credits

---
+++ credits.php
@@ -0,0 +1,28 @@
+<?php include("head.php"); ?>
+    <body>
+    <h1>À propos</h1>
+
+    <h2>Qui sommes-nous ?</h2>
+    <br>
+    <p>MathsQcmApp est un projet du groupe local IREM "Aurillac Lycée", groupe de recherche de l'IREM de Clermont-Ferrand. Ce projet a été initié durant l'année scolaire 2022-2023.</p>
+
+    <p>Les membres du groupe, à cette date sont :</p>
+    <ul>
+        <li>Patrick De Giovanni, lycée Jean Monnet (Aurillac</li>
+        <li>Fabrice Lallemand, lycée Emile Duclaux (Aurillac)</li>     
+        <li>Jérôme Mathieu, lycée Jean Monnet (Aurillac)</li>
+        <li>Alexandre Rocq, lycée Emile Duclaux (Aurillac)</li>
+        <li>Stéphane Sobella, lycée Georges Pompidou (Aurillac)</li>
+    </ul>
+
+    <h2>Ressources externes</h2>
+
+    <p>Ce site utilise plusieurs ressources référencées ci-dessous :</p>
+
+    <ul>
+        <li><a class='link' href='https://genumsi.inria.fr/index.php'>GeNumSI :</a> le point de départ du développement de cette application.</li>
+        <li><a class='link' href='https://getbootstrap.com/'>Bootstrap :</a> styles css rapides</li>
+        <li>Logo : <a class='link' href='https://pixabay.com/fr/'>Pixabay</a></li>
+    </ul>
+
+<?php include("footer.php") ?>
